275f70086807b0c492b11a5781f99c3fead69613
alechowski <lechowski.andrzej@outlook.com>
2023-07-17 23:06:07 +0200

    feat: add scrollspy function

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ const menuList = document.querySelector('.navbar__list');
 const allMenuItems = document.querySelectorAll('.navbar__item');
 const arrowsBtn = document.querySelector('.header__arrows-box');
 const footerYear = document.querySelector(".footer__year");
+const navLinks = document.querySelectorAll('.navbar__item-link');
+const scrollSpySections = document.querySelectorAll('.section');
 
 const menuAnimation = () => {
 	menuList.classList.toggle('navbar__list--active');
@@ -26,6 +28,30 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year;
 };
 
+
+const handleScrollSpy = () => {
+    if(document.body.classList.contains('main-page')) {
+
+        const sections = [] 
+
+        scrollSpySections.forEach(section => {
+
+            if(window.scrollY <= section.offsetTop + section.offsetHeight - 80) {
+                sections.push(section)
+
+                const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
+
+                navLinks.forEach(item => item.classList.remove('navbar__item-link--active'))
+
+                activeSection.classList.add('navbar__item-link--active')
+            }
+
+            })
+    }
+}
+
+
 handleCurrentYear();
 burgerBtn.addEventListener('click', menuAnimation);
 arrowsBtn.addEventListener('click', scrollFunction);
+window.addEventListener('scroll', handleScrollSpy);
